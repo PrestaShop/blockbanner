3115a84b3488ef75975e5d214877f66cc7334783
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-03-16 15:17:34 +0100

    // Version updated to v1.3.5

--- blockbanner.php
+++ blockbanner.php
@@ -33,7 +33,7 @@ class BlockBanner extends Module
 	{
 		$this->name = 'blockbanner';
 		$this->tab = 'front_office_features';
-		$this->version = '1.3.4';
+		$this->version = '1.3.5';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
